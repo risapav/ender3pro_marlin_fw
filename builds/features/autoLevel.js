18e935b07afc778b2d8d6d69dc141d8e01441418
risapav <risapav@gmail.com>
2023-02-18 14:05:54 +0100

    Update autoLevel.js

--- builds/features/autoLevel.js
+++ builds/features/autoLevel.js
@@ -2,29 +2,37 @@ module.exports = {
     partial: true,
     configuration: {
         enable: [
+            //mandatory for BLTOUCH
             "BLTOUCH",
-            ["NOZZLE_TO_PROBE_OFFSET", [-42, -10, 0]],
-            "AUTO_BED_LEVELING_BILINEAR",        
+            "AUTO_BED_LEVELING_BILINEAR",
             "Z_SAFE_HOMING",
+            //BLTOUCH settings
             "Z_MIN_PROBE_USES_Z_MIN_ENDSTOP_PIN",
+            //BLTOUCH probe offsets
+            ["NOZZLE_TO_PROBE_OFFSET", [-42, -10, 0]],
+                    
+            
+            
             ["DEFAULT_LEVELING_FADE_HEIGHT", 20]
-          
-//            "USE_PROBE_FOR_Z_HOMING",
-//            ["Z_MIN_PROBE_PIN", q`PB1`],
-//            "LCD_BED_LEVELING",
+
 
             
             
 
             //M48 test
 //            "Z_MIN_PROBE_REPEATABILITY_TEST"
+        ],
+        disable: [
+            "USE_PROBE_FOR_Z_HOMING",
+            "LCD_BED_LEVELING"
         ]
     },
     configuration_adv: {
         enable: [
-            "BABYSTEPPING",//ano
-            "BABYSTEP_ZPROBE_OFFSET",//ano
-            "PROBE_OFFSET_WIZARD",//ano
+            //for easier way to calibrate Z offset
+            "BABYSTEPPING",
+            "BABYSTEP_ZPROBE_OFFSET",
+            "PROBE_OFFSET_WIZARD",
 
             //symmetrical
             ["PROBING_MARGIN_LEFT", 45],
