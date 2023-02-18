5f2b6b99da63ba4d724ce120d3cbaf07abf7f876
risapav <risapav@gmail.com>
2023-02-18 15:11:29 +0100

    Update titan.js

--- builds/features/titan.js
+++ builds/features/titan.js
@@ -4,14 +4,6 @@ module.exports = {
         enable: [
           ["DEFAULT_AXIS_STEPS_PER_UNIT",  { 80, 80, 400, 398.087 }],
           ["INVERT_E0_DIR", true]
-        ],
-        disable: [
-
-        ]
-    },
-    configuration_adv: {
-        enable: [
-
         ]
     }
 };
