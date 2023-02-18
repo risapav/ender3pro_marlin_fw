9d0aaf9f751957815c5055be85dd277167216baf
risapav <risapav@gmail.com>
2023-02-18 15:14:30 +0100

    Update titan.js

--- builds/features/titan.js
+++ builds/features/titan.js
@@ -2,7 +2,7 @@ module.exports = {
     partial: true,
     configuration: {
         enable: [
-          ["DEFAULT_AXIS_STEPS_PER_UNIT",  { 80, 80, 400, 398.087 }],
+          ["DEFAULT_AXIS_STEPS_PER_UNIT",  [ 80, 80, 400, 398.087 ]],
           ["INVERT_E0_DIR", true]
         ]
     }
