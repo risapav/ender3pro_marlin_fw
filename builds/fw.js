4640940c63cffd6fd43f7af4160e148b3882e9d7
risapav <risapav@gmail.com>
2023-02-18 14:36:39 +0100

    Create fw.js

---
+++ builds/fw.js
@@ -0,0 +1,17 @@
+
+module.exports = {
+    extends: "builds/base.js",
+    include: {
+      "builds/features/autoLevel.js"
+    },
+    meta: {
+        stable_name: "ender_3_4.2.7-{{marlin_version}}-fw-{{uid}}",
+        nightly_name: "ender_3_4.2.7-{{current_date}}-fw-{{uid}}"
+    },
+    configuration: {
+        enable: [
+          //probing grid
+            ["GRID_MAX_POINTS_X", 3]
+        ]
+    }
+};
