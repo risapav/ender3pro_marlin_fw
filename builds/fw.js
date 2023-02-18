6aa0d596242275e1fbe7c0989a82930ab3e0580c
risapav <risapav@gmail.com>
2023-02-18 15:25:49 +0100

    Update fw.js

--- builds/fw.js
+++ builds/fw.js
@@ -1,10 +1,10 @@
 
 module.exports = {
     extends: "builds/base.js",
-    include: {
+    include: [
       "builds/features/autoLevel.js",
       "builds/features/titan.js"
-    },
+    ],
     meta: {
         stable_name: "ender_3_4.2.7-{{marlin_version}}-fw-{{uid}}",
         nightly_name: "ender_3_4.2.7-{{current_date}}-fw-{{uid}}"
