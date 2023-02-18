2e28dcdc4f97d1d11b72f6fe80105be5b65e41ca
risapav <risapav@gmail.com>
2023-02-18 15:01:18 +0100

    Create titan.js

---
+++ builds/features/titan.js
@@ -0,0 +1,17 @@
+module.exports = {
+    partial: true,
+    configuration: {
+        enable: [
+          ["DEFAULT_AXIS_STEPS_PER_UNIT",  { 80, 80, 400, 398.087 }],
+          ["INVERT_E0_DIR", true]
+        ],
+        disable: [
+
+        ]
+    },
+    configuration_adv: {
+        enable: [
+
+        ]
+    }
+};
