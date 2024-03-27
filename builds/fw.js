b2cfb2032d66063b6702e7903cd44c0025e5f896
risapav <risapav@gmail.com>
2024-03-27 20:22:10 +0100

    define printable area and offset of start point

--- builds/fw.js
+++ builds/fw.js
@@ -11,6 +11,13 @@ module.exports = {
     },
     configuration: {
         enable: [
+          // The size of the printable area
+         ["X_BED_SIZE", 225],
+         ["Y_BED_SIZE", 225],            
+          //start position
+         ["X_MIN_POS", -15],            
+         ["Y_MIN_POS", -25], 
+         ["Z_MIN_POS", 0],             
           //probing grid
          ["GRID_MAX_POINTS_X", 3]
         ]
