1736a6b2f544194e653a87257f901824564f83ba
risapav <risapav@gmail.com>
2024-03-27 21:00:11 +0100

    Update fw.js

--- builds/fw.js
+++ builds/fw.js
@@ -3,21 +3,15 @@ module.exports = {
     extends: "builds/base.js",
     include: [
       "builds/features/autoLevel.js",
-      "builds/features/titan.js"
+      "builds/features/titan.js",
+      "builds/features/positioning.js"
     ],
     meta: {
         stable_name: "ender_3_4.2.7-{{marlin_version}}-fw-{{uid}}",
         nightly_name: "ender_3_4.2.7-{{current_date}}-fw-{{uid}}"
     },
     configuration: {
-        enable: [
-          // The size of the printable area
-         ["X_BED_SIZE", 225],
-         ["Y_BED_SIZE", 225],            
-          //start position
-         ["X_MIN_POS", -15],            
-         ["Y_MIN_POS", -25], 
-         ["Z_MIN_POS", 0],             
+        enable: [           
           //probing grid
          ["GRID_MAX_POINTS_X", 3]
         ]
