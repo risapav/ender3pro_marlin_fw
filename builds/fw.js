f18f8348c75fd185a8da687dc39d3c28a6a97cdd
risapav <risapav@gmail.com>
2024-03-28 11:36:13 +0100

    Update fw.js

--- builds/fw.js
+++ builds/fw.js
@@ -10,16 +10,9 @@ module.exports = {
         nightly_name: "ender_3_4.2.7-{{current_date}}-fw-{{uid}}"
     },
     configuration: {
-        enable: [       
-          // The size of the printable area
-//       ["X_BED_SIZE", 225],
-//        ["Y_BED_SIZE", 220],            
-         //start position
-//        ["X_MIN_POS", -15],            
-//        ["Y_MIN_POS", -25],
-//        ["Z_MIN_POS", 0],        
+        enable: [             
           //probing grid
-         ["GRID_MAX_POINTS_X", 3]
+          ["GRID_MAX_POINTS_X", 3]
         ]
     }
 };
